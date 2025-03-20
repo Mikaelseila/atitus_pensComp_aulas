34308e9bcd610600462de8aa3582effae4cd655d
Mikaelseila <meka8970@gmail.com>
2025-03-19 21:45:00 -0300

    fazendo a raiz funcionar .-. 👍

--- 03_variaveis_e_operadores_matematicos/raiz_quadrada.py
+++ 03_variaveis_e_operadores_matematicos/raiz_quadrada.py
@@ -1,8 +1,10 @@
 def raiz_quadrada(valor):
-    # Preencher
-    pass
+    return valor  ** (1/2)
 
+def test():
+    assert raiz_quadrada(9) == 3
+    assert raiz_quadrada(16) == 4
+    assert raiz_quadrada(25) == 5
 
-assert raiz_quadrada(9) == 3
-assert raiz_quadrada(16) == 4
-assert raiz_quadrada(25) == 5
+print(raiz_quadrada(49))
+
