ba4710deeec5b7e1f71d96e5c25e94c92ca65fa5
Mikaelseila <meka8970@gmail.com>
2025-04-07 19:59:57 -0300

    Update fatorial.py

--- 05_Listas,_tuplas_e_lacos_de_repeticao/fatorial.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/fatorial.py
@@ -1,12 +1,19 @@
-def fatorial(numero):
-    # Preencher
-    pass
+def fatorial(numerozin):
+ if numerozin == 0:
+      return 1
+ elif numerozin ==-1:
+     return None
+ else:
+      return numerozin * fatorial(numerozin - 1)
 
+numerozin = int(input("adicione um número: "))
+print (fatorial(numerozin))
 
-assert fatorial(0) == 1
-assert fatorial(1) == 1
-assert fatorial(2) == 2
-assert fatorial(3) == 6
-assert fatorial(4) == 24
-assert fatorial(5) == 120
-assert fatorial(-1) is None
+def test():
+ assert fatorial(0) == 1
+ assert fatorial(1) == 1
+ assert fatorial(2) == 2
+ assert fatorial(3) == 6
+ assert fatorial(4) == 24
+ assert fatorial(5) == 120
+ assert fatorial(-1) is None
