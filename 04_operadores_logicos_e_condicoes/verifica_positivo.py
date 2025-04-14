344e83f5afb8715dfb69013623724970b7e6d90e
Mikaelseila <meka8970@gmail.com>
2025-04-14 19:27:47 -0300

    Update verifica_positivo.py

--- 04_operadores_logicos_e_condicoes/verifica_positivo.py
+++ 04_operadores_logicos_e_condicoes/verifica_positivo.py
@@ -1,21 +1,29 @@
+numero = int(input("digite um número: "))
 def eh_positivo(numero):
-    pass
+    if numero >=0:
+        print("o número é positivo")
+    else:
+        print(eh_negativo(numero))
+
 
 
 def eh_negativo(numero):
-    pass
+    if numero <=-1:
+        print("o número é negativo")
+    else:
+        print(eh_positivo(numero))
 
+def test():
+ assert eh_positivo(1)
+ assert eh_positivo(2)
+ assert eh_positivo(10)
+ assert not eh_positivo(0)
+ assert not eh_positivo(-1)
+ assert not eh_positivo(-10)
 
-assert eh_positivo(1)
-assert eh_positivo(2)
-assert eh_positivo(10)
-assert not eh_positivo(0)
-assert not eh_positivo(-1)
-assert not eh_positivo(-10)
-
-assert eh_negativo(-1)
-assert eh_negativo(-2)
-assert eh_negativo(-10)
-assert not eh_negativo(0)
-assert not eh_negativo(1)
-assert not eh_negativo(10)
+ assert eh_negativo(-1)
+ assert eh_negativo(-2)
+ assert eh_negativo(-10)
+ assert not eh_negativo(0)
+ assert not eh_negativo(1)
+ assert not eh_negativo(10)
