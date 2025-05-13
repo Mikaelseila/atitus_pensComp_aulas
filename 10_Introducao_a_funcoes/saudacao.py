3b3a5e9efa78e26b7ecc38a1cb8b29e46719e281
Mikaelseila <meka8970@gmail.com>
2025-05-12 22:12:55 -0300

    Update saudacao.py

--- 10_Introducao_a_funcoes/saudacao.py
+++ 10_Introducao_a_funcoes/saudacao.py
@@ -1,9 +1,14 @@
-def saudacao(nome, sobrenome, titulo):
-    pass
+def saudacao(nome, sobrenome = None, titulo = "Sr."):
+    if nome is "":
+        return "Olá!"
+    elif sobrenome is None:
+        return f"Olá, {titulo} {nome}"
+    else:
+        return f"Olá, {titulo} {nome} {sobrenome}"
 
-
-assert saudacao("Matheus") == "Olá, Matheus"
-assert saudacao("Matheus", "Jardim") == "Olá, Matheus Jardim"
-assert saudacao("Matheus", "Jardim", "Prof") == "Olá, Prof Matheus Jardim"
-assert saudacao("Matheus", titulo="Prof") == "Olá, Prof Matheus"
-assert saudacao("") == "Olá!"
+def test():
+ assert saudacao("Matheus") == "Olá, Sr. Matheus"
+ assert saudacao("Matheus", "Jardim") == "Olá, Sr. Matheus Jardim"
+ assert saudacao("Matheus", "Jardim", "Prof") == "Olá, Prof Matheus Jardim"
+ assert saudacao("Matheus", titulo="Prof") == "Olá, Prof Matheus"
+ assert saudacao("") == "Olá!"
