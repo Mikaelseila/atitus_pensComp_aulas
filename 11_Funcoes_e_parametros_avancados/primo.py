0071c607ce0c4b2233a0d807ce48b0d021587035
Mikaelseila <meka8970@gmail.com>
2025-05-14 21:24:30 -0300

    Update primo.py

--- 11_Funcoes_e_parametros_avancados/primo.py
+++ 11_Funcoes_e_parametros_avancados/primo.py
@@ -1,11 +1,23 @@
+from sympy import false
+
+
 def eh_primo(numero: int) -> bool:
-    pass
+        if numero <= 1: #se o número for menor ou igual ao 1, ele não é primo
+            return False
+        if numero > 1:
+            for i in range(2, numero): #calcula cada número verdadeiro entre 2 e o número digitado
+                if numero % i == 0: #se o resultado da divisão entre o número e o i for 0, ele não é primo
+                    return False
+            else:
+                return True #caso o contrário, ele é um número primo.
 
 
-assert not eh_primo(-1)
-assert not eh_primo(0)
-assert not eh_primo(1)
-assert eh_primo(2)
-assert eh_primo(3)
-assert not eh_primo(4)
-assert eh_primo(5)
+
+def test():
+ assert not eh_primo(-1)
+ assert not eh_primo(0)
+ assert not eh_primo(1)
+ assert eh_primo(2)
+ assert eh_primo(3)
+ assert not eh_primo(4)
+ assert eh_primo(5)
