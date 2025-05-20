478f6567a95ed407e28a566c97b46a01450b7d4a
Mikaelseila <meka8970@gmail.com>
2025-05-20 20:48:40 -0300

    Update cifra_cesar.py

--- 08_Strings_random/cifra_cesar.py
+++ 08_Strings_random/cifra_cesar.py
@@ -1,11 +1,31 @@
 def caesar_cipher(texto, desvio):
-    pass
+    result = ""
+    for char in texto:
+        if char.isalpha():
+            if char.isupper():
+                ascii_offset = 65
+            else:
+                ascii_offset = 97
+            shifted_char = chr((ord(char) - ascii_offset + desvio) % 26 + ascii_offset)
+            result += shifted_char
+        else:
+         result += char
+    return result
 
+desvio = 3
+texto = "Hello, World!"
+print("Original:", texto)
 
-assert caesar_cipher("Hello, World!", 3) == "Khoor, Zruog!"
-assert caesar_cipher("Khoor, Zruog!", -3) == "Hello, World!"
+bill_cipher = caesar_cipher(texto, desvio)
+print ("Codificado:", bill_cipher)
 
-assert caesar_cipher("Matheus Jardim", 3) == "Pdwkhxv Mduglp"
-assert caesar_cipher("Pdwkhxv Mduglp", -3) == "Matheus Jardim"
+textin = caesar_cipher(bill_cipher, -desvio)
+print("decifradin:", texto)
+def test():
+ assert caesar_cipher("Hello, World!", 3) == "Khoor, Zruog!"
+ assert caesar_cipher("Khoor, Zruog!", -3) == "Hello, World!"
 
-assert caesar_cipher(caesar_cipher("Atitus", 3), -3) == "Atitus"
+ assert caesar_cipher("Matheus Jardim", 3) == "Pdwkhxv Mduglp"
+ assert caesar_cipher("Pdwkhxv Mduglp", -3) == "Matheus Jardim"
+
+ assert caesar_cipher(caesar_cipher("Atitus", 3), -3) == "Atitus"
