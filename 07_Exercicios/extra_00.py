7939dcdb54c18e4588ba6a3a7bfa7d0c08131d4b
Mikaelseila <meka8970@gmail.com>
2025-05-20 21:31:11 -0300

    Update extra_00.py

--- 07_Exercicios/extra_00.py
+++ 07_Exercicios/extra_00.py
@@ -2,17 +2,19 @@ ANO_ATUAL = 2024
 
 
 def saudacao(nome, sobrenome, ano_nascimento):
-    # Preencher
-    pass
+    if ano_nascimento <= 0 or ano_nascimento > ANO_ATUAL:
+     return None
+    idade = ANO_ATUAL - ano_nascimento
+    return f"Olá, {nome} {sobrenome}. Bom dia! Você possui {idade} anos!"
 
-
-assert (
+def test():
+ assert (
     saudacao("Matheus", "Jardim", 1991)
     == "Olá, Matheus Jardim. Bom dia! Você possui 33 anos!"
-)
-assert (
+ )
+ assert (
     saudacao("Thais", "Silva", 1990)
     == "Olá, Thais Silva. Bom dia! Você possui 34 anos!"
-)
-assert saudacao("Matheus", "Jardim", 0) is None
-assert saudacao("Matheus", "Jardim", 2050) is None
+ )
+ assert saudacao("Matheus", "Jardim", 0) is None
+ assert saudacao("Matheus", "Jardim", 2050) is None
