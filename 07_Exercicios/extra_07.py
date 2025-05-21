5744328777e6910970528a7d5049708a05630b7f
Mikaelseila <meka8970@gmail.com>
2025-05-20 21:37:18 -0300

    Update extra_07.py

--- 07_Exercicios/extra_07.py
+++ 07_Exercicios/extra_07.py
@@ -1,15 +1,15 @@
-def fahrenheit_para_celsius(valor):
-    pass
+def fahrenheit_para_celsius(fah):
+    cel = (fah -32) / 1.8
+    return cel
 
 
-def celsius_para_fahrenheit(valor):
-    pass
+def celsius_para_fahrenheit(cel):
+    fah = (1.8 * cel) + 32
+    return fah
 
 
-assert fahrenheit_para_celsius(104) == 40
-assert fahrenheit_para_celsius(-13) == -25
+def test():
+ assert celsius_para_fahrenheit(40) == 104
+ assert celsius_para_fahrenheit(-25) == -13
 
-assert celsius_para_fahrenheit(40) == 104
-assert celsius_para_fahrenheit(-25) == -13
-
-assert celsius_para_fahrenheit(fahrenheit_para_celsius(30)) == 30
+ assert celsius_para_fahrenheit(fahrenheit_para_celsius(30)) == 30
