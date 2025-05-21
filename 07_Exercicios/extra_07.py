ff91d2b4fc4dc71e6a1266070337656926e22b90
Mikaelseila <meka8970@gmail.com>
2025-05-21 18:22:29 -0300

    Update extra_07.py

--- 07_Exercicios/extra_07.py
+++ 07_Exercicios/extra_07.py
@@ -1,10 +1,10 @@
 def fahrenheit_para_celsius(fah):
-    cel = (fah -32) / 1.8
+    cel = (fah -32) / 1.8 #os graus celsius é o grau fahrenheit - 32 divido por 1.8
     return cel
 
 
 def celsius_para_fahrenheit(cel):
-    fah = (1.8 * cel) + 32
+    fah = (1.8 * cel) + 32 #já fahrenheit é o contrário, são os graus celsius multiplicado por 1.8 e somado com 32
     return fah
 
 
