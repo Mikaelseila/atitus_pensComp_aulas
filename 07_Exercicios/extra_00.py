ec6deed7954429d62472cce9fd1d78cf1797751b
Mikaelseila <meka8970@gmail.com>
2025-05-21 17:58:47 -0300

    Update extra_00.py

--- 07_Exercicios/extra_00.py
+++ 07_Exercicios/extra_00.py
@@ -2,9 +2,9 @@ ANO_ATUAL = 2024
 
 
 def saudacao(nome, sobrenome, ano_nascimento):
-    if ano_nascimento <= 0 or ano_nascimento > ANO_ATUAL:
+    if ano_nascimento <= 0 or ano_nascimento > ANO_ATUAL: #se o ano de nascimento for igual/menor que 0 ou for acima do ano atual, ele não contará
      return None
-    idade = ANO_ATUAL - ano_nascimento
+    idade = ANO_ATUAL - ano_nascimento #a idade é igual ao ano atual subtraído com o ano de nascimento da pessoa
     return f"Olá, {nome} {sobrenome}. Bom dia! Você possui {idade} anos!"
 
 def test():
