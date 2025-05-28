bcc614b5714b5300c6009d5bcfa1c6b66ef72e7d
Mikaelseila <meka8970@gmail.com>
2025-05-28 20:22:17 -0300

    Update fibonacci.py

--- 12_Funcoes_e_imports/fibonacci.py
+++ 12_Funcoes_e_imports/fibonacci.py
@@ -1,12 +1,20 @@
 def fibonacci(x):
-    pass
+    if x <= -1:
+        return None
+    if x == 0:
+        return 0
+    if x == 1:
+        return 1
+    return fibonacci(x - 1) + fibonacci(x - 2)
 
 
-assert fibonacci(-1) is None
-assert fibonacci(0) == 0
-assert fibonacci(1) == 1
-assert fibonacci(2) == 1
-assert fibonacci(3) == 2
-assert fibonacci(4) == 3
-assert fibonacci(5) == 5
-assert fibonacci(10) == 55
+
+def test():
+ assert fibonacci(-1) is None
+ assert fibonacci(0) == 0
+ assert fibonacci(1) == 1
+ assert fibonacci(2) == 1
+ assert fibonacci(3) == 2
+ assert fibonacci(4) == 3
+ assert fibonacci(5) == 5
+ assert fibonacci(10) == 55
