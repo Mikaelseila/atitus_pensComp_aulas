52e104eed9127b36e53d6567c50e5befe5b35665
Mikaelseila <meka8970@gmail.com>
2025-06-22 19:51:08 -0300

    Update fatorial.py

--- 12_Funcoes_e_imports/fatorial.py
+++ 12_Funcoes_e_imports/fatorial.py
@@ -1,11 +1,23 @@
 def fatorial_rec(n):
     # Com recursividade
-    pass
+     if n == 0: # Caso o número seja 0, retorna 1, abaixo disso ele retorna None
+        return 1
+     elif n < 0:
+        return None
+     else:
+        return n * fatorial_rec(n - 1) # retorna recursivamente a função (1 * 0 = 1, 2 * 1 = 2, e assim por diante)
 
 
 def fatorial_non_rec(n):
     # Sem recursividade
-    pass
+    if n == 0:
+        return 1
+    elif n < 0:
+        return None
+    result = 1
+    for i in range(1, n + 1): # Pra cada número a partir de 1, faça o fatorial (se result for 1 e i for 1, 1 * 1 = 1; 1 * 2 = 2; 2 * 3 = 6, e assim até acabar)
+        result = result * i
+    return result
 
 
 # fatorial(5) = 5  * fatorial(4)
