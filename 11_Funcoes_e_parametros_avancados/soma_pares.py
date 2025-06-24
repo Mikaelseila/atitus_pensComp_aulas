90cb5223d725b08b54b1c0af0048705d24419204
Mikaelseila <meka8970@gmail.com>
2025-06-24 16:35:51 -0300

    Update soma_pares.py

--- 11_Funcoes_e_parametros_avancados/soma_pares.py
+++ 11_Funcoes_e_parametros_avancados/soma_pares.py
@@ -1,9 +1,18 @@
 def soma_pares(numeros: list, alvo: int) -> bool:
-    pass
+    somar = []
+    for numero in numeros:
+        somar.append(numero)
+
+    for n in range(len(somar)):
+        for j in range(len(somar)):
+            if somar[n] + somar[j] == alvo:
+                return True
+    return False
 
 
-assert soma_pares([1, 2], 4)
-assert not soma_pares([8], 1)
-assert not soma_pares([8], 10)
-assert soma_pares([3, 4, 6], 9)
-assert soma_pares([3, 4, 6], 7)
+def test():
+    assert soma_pares([1, 2], 4)
+    assert not soma_pares([8], 1)
+    assert not soma_pares([8], 10)
+    assert soma_pares([3, 4, 6], 9)
+    assert soma_pares([3, 4, 6], 7)
